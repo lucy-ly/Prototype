d5fab6fc0ca11b2bee66bde44dadbaf48de6dcf3
Lucy Ly <lucyly.student@careerdevs.com>
2019-06-08 16:03:08 +0000

    added prototypeNewObject.js

---
+++ prototypeNewObject.js
@@ -0,0 +1,25 @@
+function Dog(name) {
+    this.name = name;
+}
+
+Dog.prototype = {
+    // Add your code below this line
+    numLegs: 2,
+    eat: function() {
+        console.log("nom nom nom");
+    },
+    describe: function() {
+        console.log("my name is " + this.name)
+    }
+};
+
+
+// Bird.prototype = {
+//   numLegs: 2, 
+//   eat: function() {
+//     console.log("nom nom nom");
+//   },
+//   describe: function() {
+//     console.log("My name is " + this.name);
+//   }
+// };
